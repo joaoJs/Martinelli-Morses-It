9147607929e67fc79aad81ca6c7a3278dd1c132f
Joao Campos <j.campos4g@gmail.com>
2017-11-13 16:26:56 -0500

    asks for random letter and checks if correct

--- controls.js
+++ controls.js
@@ -3,13 +3,15 @@ function initializeKeyboard() {
       $(document).keydown(function(e) {
         if (e.keyCode === 32) {
           isPressed = true;
-        } if (e.keyCode === 13) {
+        } else if (e.keyCode === 13) {
           if (hasStarted) {
             hasStarted = false;
           } else {
             hasStarted = true;
             startBits();
           }
+        } else if (e.keyCode === 38) {
+          whichLetter();
         }
       });
 
