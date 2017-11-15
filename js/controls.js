69c1ee4743ef115b72ccbb1733afcadc28af245b
Joao Campos <j.campos4g@gmail.com>
2017-11-15 09:14:02 -0500

    improved game control

--- js/controls.js
+++ js/controls.js
@@ -2,26 +2,56 @@ function initializeKeyboard() {
       console.log("Keyboard initialized");
       $(document).keydown(function(e) {
         //e.preventDefault();
+
+        //if "space bar"
         if (e.keyCode === 32) {
           e.preventDefault();
-          isPressed = true;
-        } else if (e.keyCode === 13) {
-          e.preventDefault();
+          //isPressed = true;
           if (hasStarted) {
-            hasStarted = false;
-          } else {
-            hasStarted = true;
-            startBits();
+            isPressed = true;
+            canFinish = true;
           }
-        } else if (e.keyCode === 38) {
+        }
+        // if "Enter"
+        else if (e.keyCode === 13) {
           e.preventDefault();
+          if (canStart) {
+            if (!hasStarted) {
+              hasStarted = true;
+              startBits();
+            } else if (canFinish) {
+              hasStarted = false;
+              finishBits(gameLoop);
+            }
+          }
+          // if (canStart) {
+          //   hasStarted = false;
+          //   if (canFinish) {
+          //     //hasStarted = false;
+          //     finishBits();
+          //   }
+          // }
+          // if (!hasStarted) {
+          //     hasStarted = true;
+          //     startBits();
+          // }
+        }
+
+        // if "Up arrow"
+        else if (e.keyCode === 38) {
+          e.preventDefault();
+          canStart = true;
+          hasStarted = false;
           const val = $('.result').val();
           $('.result').html("");
+          $('.start').html("");
           whichLetter();
         }
       });
 
       $(document).keyup(function(e) {
+
+        // if release "space bar"
         if (e.keyCode === 32) {
           e.preventDefault();
           isPressed = false;
